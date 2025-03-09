5b118c34f573bec0de7343e2424ece7702c4b282
koloiprank <koloiprank23@gmail.com>
2025-03-09 06:22:42 +0100

    minor map path styling hotfixes

--- assets/scripts/terramap.js
+++ assets/scripts/terramap.js
@@ -1,10 +1,20 @@
 const regions = document.querySelectorAll( '[id^="map-"]' );
 
 regions.forEach(region=>{
-    // Move region to last child when hovered to allow hovering over other regions
-    region.addEventListener('mouseenter', () => {
-        const parent = region.parentNode;
-        parent.removeChild(region);
-        parent.appendChild(region);
-  });
+    // Add special style depending of region
+    if(region.id === "map-unknown"){
+        region.style.stroke = "#babec4";
+        region.style.strokeWidth = "3.5px";
+    }
+    else{
+        region.id !== "map-aegir" ? region.style.stroke = "#333333" : true;
+        region.id !== "map-aegir" ? region.style.strokeWidth = "3.5px": true;
+
+        // Move region to last child when hovered to allow hovering over other regions
+        region.addEventListener('mouseenter', () => {
+            const parent = region.parentNode;
+            parent.removeChild(region);
+            parent.appendChild(region);
+        });
+    };
 });
