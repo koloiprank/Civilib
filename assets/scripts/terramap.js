7f59526101ee3b7a4680f9aa2ac63315fcffea50
koloiprank <koloiprank23@gmail.com>
2025-03-09 20:26:55 +0100

    Added text to map factions

--- assets/scripts/terramap.js
+++ assets/scripts/terramap.js
@@ -1,4 +1,21 @@
 const regions = document.querySelectorAll( '[id^="map-"]' );
+const parent = regions[0].parentNode
+const regionsOrdered = Array.from(parent.children);
+
+function seamlessHover(region){
+    // Move region to last
+    region.addEventListener('mouseenter', ()=>{
+        parent.appendChild(region);
+    });
+
+    // Restore original region order to avoid overlapping
+    region.addEventListener('mouseleave', ()=>{
+        setTimeout(()=>{
+            regions.forEach(r=>r.remove());
+            regionsOrdered.forEach(r=>parent.appendChild(r));
+        }, 150);
+    });
+};
 
 regions.forEach(region=>{
     // Add special style depending of region
@@ -7,14 +24,13 @@ regions.forEach(region=>{
         region.style.strokeWidth = "3.5px";
     }
     else{
-        region.id !== "map-aegir" ? region.style.stroke = "#333333" : true;
-        region.id !== "map-aegir" ? region.style.strokeWidth = "3.5px": true;
+        // Add stroke if not aegir faction
+        region.id !== "map-aegir" ? region.style.stroke = "#333333" : null;
+        region.id !== "map-aegir" ? region.style.strokeWidth = "3.5px": null;
 
         // Move region to last child when hovered to allow hovering over other regions
-        region.addEventListener('mouseenter', () => {
-            const parent = region.parentNode;
-            parent.removeChild(region);
-            parent.appendChild(region);
-        });
+        // unless its one of the non-hovereable
+        ["map-unclaimed", "map-unknown", "map-icefield", "map-unknown"].includes(region.id) ? null
+        : seamlessHover(region);
     };
 });
